24c14ecd5b08ef453c5bcacbb39e0fabd73a2c58
Raehan <raehan900809@gmail.com>
2021-03-15 19:12:08 +0900

    Component Styling

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,9 +1,11 @@
 import SassComponent from './SassComponent'
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div className="App">
       <SassComponent />
+      <StyledComponent />
     </div>
   );
 }
