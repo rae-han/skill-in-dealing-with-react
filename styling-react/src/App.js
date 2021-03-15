d05230eeec0f4e65efb3858c084f2bb0a7abbaa3
Raehan <raehan900809@gmail.com>
2021-03-15 16:35:46 +0900

    Commit before npm i eject

---
+++ styling-react/src/App.js
@@ -0,0 +1,11 @@
+import SassComponent from './SassComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ styling-react/src/SassComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SassComponent.scss'
+
+const SassComponent = () => {
+  return (
+    <div>
+      <div className="SassComponent">
+        <div className="box red">r</div>
+        <div className="box blue">b</div>
+        <div className="box yellow">y</div>
+      </div>
+    </div>
+  );
+};
+
+export default SassComponent;
