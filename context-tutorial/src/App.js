32d663aea20cb05ddf5eac2c414c8e207577db6d
Raehan <raehan900809@gmail.com>
2021-03-18 19:40:02 +0900

    Create context api

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import ColorBox from './components/ColorBox'
+import ColorContext from './contexts/color'
+
+function App() {
+  return (
+    <div className="App">
+      <ColorContext.Provider value={{ color: 'red' }}>
+        <ColorBox />
+      </ColorContext.Provider>
+    </div>
+  );
+}
+
+export default App;
