e757ee1c46243d387ba4c7809357e6e920fcac71
Raehan <raehan900809@gmail.com>
2021-03-19 21:04:22 +0900

    Add react redux container

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import Counter from '../components/Counter'
 
-const CounterContainer = () => {
+const CounterContainer = ({ number, increase, decrease }) => {
   return (
     <div>
-      <Counter/>
+      <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
     </div>
   );
 };
 
-export default CounterContainer;
+const mapStateToProps = state => ({
+  number: state.counter.number,
+})
+
+const mapDispatchToProps = dispatch => ({
+  increase: () => {
+    console.log('inc')
+  },
+  decrease: () => {
+    console.log('dec')
+  }
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CounterContainer)
+
+
+// export default CounterContainer;
