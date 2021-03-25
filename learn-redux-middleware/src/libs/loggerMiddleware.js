fb1f5a49d2d65f2539e85c10dfeb4562a9e9cfce
Raehan <raehan900809@gmail.com>
2021-03-25 21:43:00 +0900

    Add redux-logger

--- learn-redux-middleware/src/libs/loggerMiddleware.js
+++ learn-redux-middleware/src/libs/loggerMiddleware.js
@@ -1,5 +1,5 @@
 const loggerMiddleware = store => next => action => {
-  console.group(action && action.type)
+  console.group(action?.type)
   console.log('이전상태', store.getState());
   console.log('액션', action)
   next(action) // 다음 미들웨어 혹은 리듀서에게 전달
