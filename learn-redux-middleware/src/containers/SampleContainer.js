09a461e2a7b0ac30d7dbdb1e5065128b5aac1a9e
Raehan <raehan900809@gmail.com>
2021-03-26 22:08:25 +0900

    Modify sample container

--- learn-redux-middleware/src/containers/SampleContainer.js
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import Sample from '../components/Sample';
 import { getPost, getUsers } from '../modules/sample'
@@ -11,14 +11,14 @@ const SampleContainer = () => {
   const { GET_POST: loadingPost, GET_USERS: loadingUsers } = useSelector(state => state.sample.loadding);
 
   const dispatch = useDispatch();
-  const fetchPost = () => (dispatch(getPost()));
-  const fetchUsers = () => (dispatch(getUsers()));
+  const fetchPost = useCallback(id => (dispatch(getPost(id))), [dispatch]) 
+  const fetchUsers = useCallback(id => (dispatch(getUsers(id))), [dispatch])
 
 
   useEffect(() => {
     fetchPost(1);
     fetchUsers(1);
-  })
+  }, [fetchPost, fetchUsers])
 
   return (
     <div>
