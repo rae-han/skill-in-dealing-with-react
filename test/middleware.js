1916ff3ea947e9d10a0ae852aae38e977c9fcffe
Raehan <raehan900809@gmail.com>
2021-03-26 16:25:03 +0900

    Add example redux-thunk

--- test/middleware.js
+++ test/middleware.js
@@ -7,3 +7,22 @@ const loggerMiddleware = first => second => third => {
 }
 
 loggerMiddleware(1)(2)(3)
+
+const addOne1 = x => x+1;
+let result1 = addOne1(1);
+console.log(result1);
+
+setTimeout(() => {
+  const result2 = addOne1(1);
+  console.log(result2)
+}, 1000)
+
+const addOneThunk1 = (x) => {
+  setTimeout(() => {
+    const result3 = addOne1(x);
+    console.log(result3)
+  }, 1000)
+}
+
+addOneThunk1(1);
+
