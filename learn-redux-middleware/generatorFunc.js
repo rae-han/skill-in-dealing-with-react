45622868e7d7c863fcaf27da934efcf1235f24a6
Raehan <raehan900809@gmail.com>
2021-04-01 15:27:16 +0900

    Create generator file

---
+++ learn-redux-middleware/generatorFunc.js
@@ -0,0 +1,46 @@
+/** */
+// function* generatorFunc() {
+//   console.log('Hello')
+//   yield 1;
+//   console.log('Generator Fuc')
+//   yield 2;
+//   console.log('function*')
+//   yield 3;
+
+//   return 4;
+// }
+
+// const generator = generatorFunc();
+
+// console.log(generator)
+// for(let i=0; i<5; i++) {
+//   generator.next();
+// }
+
+// generator.next();
+
+/** */
+// function* sumGenerator() {
+//   console.log('sumGenerator');
+//   let a = yield;
+//   let b = yield;
+//   yield a+b;
+// }
+
+// const sum = sumGenerator();
+// let sum1 = sum.next();
+// console.log(sum1)
+// let sum2 = sum.next(1);
+// console.log(sum2)
+// let sum3 = sum.next(2);
+// console.log(sum3)
+// let sum4 = sum.next();
+// console.log(sum4)
+
+
+// function* watchGenerator() {
+//   console.log('모니터링 중...');
+// }
+
+
+const a = return;
