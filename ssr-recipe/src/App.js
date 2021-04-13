479f5beb4df401d78a2bfd3f79342937dc97757b
Raehan <raehan900809@gmail.com>
2021-04-13 15:08:12 +0900

    Add server side rendering

--- ssr-recipe/src/App.js
+++ ssr-recipe/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Route } from 'react-router-dom'
 import Menu from './components/Menu';
 import RedPage from './pages/RedPage'
--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom';
+import App from './App';
+import path from 'path'
+import fs from 'fs';
 
+const manifest = JSON.parse(fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8'))
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$\.exec(key)/)
+  .map(key => `<script src="${manifest.files[key]}"></script>`)
+  .join('')
+
+//*
+const app = express();
+
+const serverRender = (req, res, next) => {
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+
+  const root = ReactDOMServer.renderToString(jsx);
+  res.send(root)
+}
+
+const serve = express.static(path.resolve('./build'), {
+  index: false
+})
+
+app.use(serve)
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log('Running on http://localhost:5000')
+})
+//*/
+/*
 const html = ReactDOMServer.renderToString(
   <div>Hello Server Side Rendering!</div>
 )
 
 console.log(html)
+//*/
