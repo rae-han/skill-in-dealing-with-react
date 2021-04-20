f89f0e531af19775a914e0aee752c6fd91fbf57a
Raehan <raehan900809@gmail.com>
2021-04-20 21:19:07 +0900

    Add redux

---
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AuthFormBlock = styled.div``;
+
+/** 
+ * 회원가입 또는 로그인 폼을 보여준다.
+ */
+const AuthForm = () => {
+  return (
+    <AuthFormBlock>
+      AuthFormBlock
+    </AuthFormBlock>
+  );
+};
+
+export default AuthForm;
