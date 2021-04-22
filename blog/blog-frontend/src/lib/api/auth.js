b5ba86b3420b3f1f39b4a394f3eebb3c35e8ba05
Raehan <raehan900809@gmail.com>
2021-04-22 13:57:05 +0900

    Add redux saga

---
+++ blog/blog-frontend/src/lib/api/auth.js
@@ -0,0 +1,21 @@
+import client from './client';
+
+export const login = ({ username, password }) => client({
+  method: 'post',
+  url: '/api/auth/login',
+  data: {
+    username,
+    password
+  }
+})
+
+export const register = ({ username, password }) => client({
+  method: 'post',
+  url: '/api/auth/register',
+  data: {
+    username,
+    password
+  }
+})
+
+export const check = () => client.get('/api/auth/check')
