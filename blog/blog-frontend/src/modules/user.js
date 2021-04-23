b2b38406bf0bfc1c252f7a5c9940d4a51dc29540
Raehan <raehan900809@gmail.com>
2021-04-23 19:43:24 +0900

    Add logout

--- blog/blog-frontend/src/modules/user.js
+++ blog/blog-frontend/src/modules/user.js
@@ -1,13 +1,15 @@
 import { createAction, handleActions } from 'redux-actions';
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, call } from 'redux-saga/effects';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, { createRequestActionTypes } from '../lib/createRequestSaga'
 
 const TEMP_SET_USER = 'user/TEMP_SET_USER';
 const [ CHECK, CHECK_SUCCESS, CHECK_FAILURE ] = createRequestActionTypes('user/CHECK');
+const LOGOUT = 'user/LOGOUT'
 
 export const tempSetUser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check)
 function checkFailureSaga() {
@@ -17,10 +19,19 @@ function checkFailureSaga() {
     console.log('localStorage is not working')
   }
 }
+export function* logoutSaga() {
+  try {
+    yield call(authAPI.logout)
+    localStorage.removeItem('user')
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
   yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initailState = {
@@ -58,6 +69,10 @@ export default handleActions({
       user: null,
       checkError: error
     }
-  }
+  },
+  [LOGOUT]: state => ({
+    ...state,
+    user: null
+  })
 }, initailState)
 
