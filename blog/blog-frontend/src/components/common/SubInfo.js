877635e30b4f39ac8265920ba99c552015eb352f
Raehan <raehan900809@gmail.com>
2021-04-28 15:51:23 +0900

    Modify SubInfo modularized

---
+++ blog/blog-frontend/src/components/common/SubInfo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+
+const SubInfoBlock = styled.div`
+  ${props => props.hasMarginTop && css`
+    margin-top: 1rem;
+  `}
+  color: ${palette.gray[6]};
+
+  span + span:before {
+    color: ${palette.gray[4]};
+    padding-left: .25rem;
+    padding-right: .25rem;
+    content: '\\B7'
+  }
+`
+
+const SubInfo = ({ username, publishedDate, hasMarginTop }) => {
+  return (
+    <SubInfoBlock hasMarginTop={hasMarginTop}>
+      <span>
+        <Link to={`/@${username}`}>{username}</Link>
+      </span>
+      <span>{new Date(publishedDate).toLocaleDateString()}</span>
+    </SubInfoBlock>
+  )
+}
---
+++ blog/blog-frontend/src/components/posts/PostList.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import styled from 'styled-components';
+import Responsive from '../common/Responsive';
+import Button from '../common/Button';
+import palette from '../../lib/styles/palette';
+
+const PostListBlock = styled(Responsive)`
+  margin-top: 3rem;
+`;
+
+const WritePostButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 3rem;
+`;
+
+const PostItemBlock = styled.div`
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  /* 맨 위 포스트는 padding-top 없음 */
+  &:first-child {
+    padding-top: 0;
+  }
+  & + & {
+    border-top: 1px solid ${palette.gray[2]};
+  }
+
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 0;
+    margin-top: 0;
+    &:hover {
+      color: ${palette.gray[6]};
+    }
+  }
+  p {
+    margin-top: 2rem;
+  }
+`;
+
+const Tags = styled.div`
+  margin-top: .5rem;
+  .tag {
+    display: inline-block;
+    color: ${palette.cyan[7]};
+    text-decoration: none;
+    margin-right: .5rem;
+    &:hover {
+      color: ${palette.cyan[6]};
+    }
+  }
+`;
+
+const PostItem = () => {
+  return (
+    <PostItemBlock>
+      <h2>제목</h2>
+      <SubInfo>
+        <span>
+          <b>username</b>
+        </span>
+        <span>{new Date().toLocaleDateString()}</span>
+      </SubInfo>
+      <Tags>
+        <div className="tag">tag</div>
+      </Tags>
+      <p>content</p>
+    </PostItemBlock>
+  )
+}
+
+
+const PostList = () => {
+  return (
+    <PostListBlock>
+      <WritePostButtonWrapper>
+        <Button cyan to="/write">
+          새 글 작성하기
+        </Button>
+      </WritePostButtonWrapper>
+      <div>
+        <PostItem />
+        <PostItem />
+        <PostItem />
+      </div>
+    </PostListBlock>
+  );
+};
+
+export default PostList;
